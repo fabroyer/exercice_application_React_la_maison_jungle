0ccafadfb065790540b34c0e2aa79bf7b78ef466
Fabien Royer <royer.tech@outlook.com>
2026-05-11 21:42:21 +0200

    PlantItem2

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,6 +1,12 @@
+import Sun from '../assets/sun.svg'
+import Water from '../assets/water.svg'
+
 function CareScale({ scaleValue, careType }) {
 	const range = [1, 2, 3]
-	const scaleType = careType === 'light' ? '☀️' : '💧'
+	const scaleType =
+	    careType === 'light' ?
+            (<img src={Sun} alt='sun-icon' />) :
+            (<img src={Water} alt='water-icon' />)
 
 	return (
 		<div>
